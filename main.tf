bf84350d839d8d3fb81e50fbcdb9245c1aa85911
Marcus Rath <marcus@matrixpost.de>
2026-05-23 18:19:20 +0200

    Update main Terraform backend configuration

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-backend"
-    storage_account_name = "sttfbackenddemo2026"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-  }
+  backend "azurerm" {}
 }
 
 resource "azurerm_resource_group" "rg_demo_tf_backend" {
